4c74324567660a2cd2204d9e5d69123038655b4c
GREngineer <114416978+GREngineer@users.noreply.github.com>
2025-06-14 00:14:41 +0300

    feat() employee crud functions

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,8 +1,55 @@
 const employees = require('../models/employeeModel');
+const { v4: uuidv4 } = require('uuid');
+
+const empNotFoundMessage = 'Employee not found';
 
 // Controller function to get all employees
 const getAllEmployees = (request, response) => {
     response.json(employees);
 };
 
-module.exports = { getAllEmployees };
+// Function to create a new employee
+const createEmployee = (request, response) => {
+    // Get the employee data from the request body
+    const newEmployee = request.body;
+    // Generate unique id
+    const employeeId = uuidv4();
+    // Add to array
+    const createdEmployee = { ...newEmployee, id: employeeId };
+    employees.push(createdEmployee);
+    // Send back newly created employee with success status
+    response.status(201).json(createdEmployee);
+};
+
+// Function to update existing employee
+const updateEmployee = (request, response) => {
+    // Get employee id from URL
+    const employeeId = request.params.id;
+    // Get the updated data from request body
+    const updatedData = request.body;
+    // Find employee in array
+    const employeeIndex = employees.findIndex(emp => emp.id === employeeId);
+    // Return error when employee doesn't exist
+    if (employeeIndex === -1) {
+        return response.status(404).json({ message: empNotFoundMessage });
+    }
+    // Update emplooyee while keeping original id
+    employees[employeeIndex] = { ...employees[employeeIndex], ...updatedData, id: employeeId };
+    // Send back updated employee
+    response.json(employees[employeeIndex]);
+};
+
+// Employee deletion function
+const deleteEmployee = (request, response) => {
+    const employeeId = request.params.id;
+    const employeeIndex = employees.findIndex(emp => emp.id === employeeId);
+    if (employeeIndex === -1) {
+        return response.status(404).json({ message: empNotFoundMessage});
+    }
+    // Remove employee from array
+    employees.splice(employeeIndex, 1);
+    // Return success message
+    response.status(200).json({ message: 'Employee deleted successfully'})
+}
+
+module.exports = { getAllEmployees, createEmployee, updateEmployee, deleteEmployee }; 
--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getAllEmployees } = require('../controllers/employeeController');
+const { getAllEmployees, createEmployee, updateEmployee, deleteEmployee } = require('../controllers/employeeController');
 
 router.get('/', getAllEmployees);
+router.post('/', createEmployee);
+router.put('/:id', updateEmployee);
+router.delete('/:id', deleteEmployee)
 
 module.exports = router;
