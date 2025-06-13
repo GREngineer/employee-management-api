83ecafe75a5c84ef860ff04042f1d4a0817bb3e9
GREngineer <114416978+GREngineer@users.noreply.github.com>
2025-06-13 20:18:50 +0300

    feat() setup first employee controller, model, url

---
+++ models/employeeModel.js
@@ -0,0 +1,8 @@
+// Employees Array
+
+const employees = [
+    { id: 1, name: "Maria", surname: "Papadopoulou", skills: ["Accounting"] },
+    { id: 2, name: "Nikolas", surname: "Eustathiou", skills: ["Audit"] }
+];
+
+module.exports = employees;
